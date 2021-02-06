e260a357ef686f077c261dc096653b3ea5b8f9e0
Probo Scis <ruthworku101@gmail.com>
2021-02-06 08:46:45 +0000

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point into a function that prints single digit numbers
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = '0';
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+
+	return (0);
+}
