fe49d693be1d7073a080acf1972357f1d4a3dd2a
Probo Scis <ruthworku101@gmail.com>
2021-02-09 19:04:40 +0000

    better 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,7 +4,6 @@
  * main - causes an infinite loop
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
@@ -13,7 +12,8 @@ int main(void)
 
 	i = 0;
 
-	/*while (i < 10)
+	/*
+	 *while (i < 10)
 	 *{
 	 *	putchar(i);
 	 *}
