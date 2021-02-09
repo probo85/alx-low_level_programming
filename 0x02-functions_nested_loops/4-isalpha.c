ddb46d7ca08af57086b90b8ab42da6ce4ccfa63c
Probo Scis <ruthworku101@gmail.com>
2021-02-09 06:18:25 +0000

    add 4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - checks if an argument is a letter
+ *
+ * c - parameter of type int
+ *
+ * Return: 1 if c is a letter and 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int num;
+
+	for (num = 0; num < 52; num++)
+	{
+		if (alpha[num] == c)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
