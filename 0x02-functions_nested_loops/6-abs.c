0a92780a8ddee566621a4f14d06f0695b0df1300
Probo Scis <ruthworku101@gmail.com>
2021-02-09 07:20:04 +0000

    add 6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _abs - compute the absolute value of an integer
+ * i - integer type
+ * Return: positive integer or 0
+ */
+int _abs(int i)
+{
+	if (i <= 0)
+		return (i * -1);
+	return (i);
+}
