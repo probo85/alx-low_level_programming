836c956a97bc6e450332d08408a27d20b4d97d1e
Probo Scis <ruthworku101@gmail.com>
2021-02-10 08:48:56 +0000

    making betty happy about the 24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,29 +7,29 @@
 void jack_bauer(void)
 {
 	int h1;
-        int h2;
-    	int m1;
-    	int m2;
+	int h2;
+	int m1;
+	int m2;
 
 	for (h1 = '0'; h1 <= '2'; h1++)
 	{
-	for (h2 = '0'; h2 <= '9'; h2++)
-	{
-	    for (m1 = '0'; m1 <= '5'; m1++)
-	    {
-		for (m2 = '0'; m2 <= '9'; m2++)
+		for (h2 = '0'; h2 <= '9'; h2++)
 		{
-		    _putchar(h1);
-		    _putchar(h2);
-		    _putchar(':');
-		    _putchar(m1);
-		    _putchar(m2);
-		    _putchar('\n');
-		}
-	    }
+			for (m1 = '0'; m1 <= '5'; m1++)
+			{
+				for (m2 = '0'; m2 <= '9'; m2++)
+				{
+					_putchar(h1);
+					_putchar(h2);
+					_putchar(':');
+					_putchar(m1);
+					_putchar(m2);
+					_putchar('\n');
+				}
+			}
 
-	    if (h1 == '2' && h2 == '3')
-		break;
-	}
+			if (h1 == '2' && h2 == '3')
+				break;
+		}
 	}
 }
