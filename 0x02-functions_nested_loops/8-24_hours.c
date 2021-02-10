f442ced624c4d3da6898f9e4550291db8e5261ee
probo85 <77930180+probo85@users.noreply.github.com>
2021-02-10 10:50:33 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,17 +6,30 @@
  */
 void jack_bauer(void)
 {
-	int i;
-	int j;
+	int h1;
+        int h2;
+    	int m1;
+    	int m2;
 
-	for (i = '00'; i < '24'; i++)
+	for (h1 = '0'; h1 <= '2'; h1++)
 	{
-		for (j = '00'; j <= '59'; j++)
+	for (h2 = '0'; h2 <= '9'; h2++)
+	{
+	    for (m1 = '0'; m1 <= '5'; m1++)
+	    {
+		for (m2 = '0'; m2 <= '9'; m2++)
 		{
-			_putchar(i);
-			_putchar(':');
-			_putchar(j);
-			_putchar('\n');
+		    _putchar(h1);
+		    _putchar(h2);
+		    _putchar(':');
+		    _putchar(m1);
+		    _putchar(m2);
+		    _putchar('\n');
 		}
+	    }
+
+	    if (h1 == '2' && h2 == '3')
+		break;
+	}
 	}
 }
