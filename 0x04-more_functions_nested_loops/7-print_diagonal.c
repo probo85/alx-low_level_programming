eaf5e6cfc2d4dc06cbbee068724cb941dc659e54
Probo Scis <ruthworku101@gmail.com>
2021-02-12 18:44:38 +0000

    add diagonal shapes

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - draw a diagonal line
+ * @n: number of times the character \ should be printed
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (i >= 1)
+			{
+				for (j = 0; j < i; j++)
+				{
+					_putchar(' ');
+				}
+			}
+
+			_putchar('\\');
+			_putchar('\n');
+		}
+
+		_putchar('\n');
+	}
+}
