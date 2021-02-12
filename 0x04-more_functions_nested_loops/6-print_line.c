43f64fedeec73f21e938cc051c38f01fddd4092e
Probo Scis <ruthworku101@gmail.com>
2021-02-12 18:05:19 +0000

    add drawing a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: the number of _ character to be printed
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+
+		_putchar('\n');
+	}
+}
